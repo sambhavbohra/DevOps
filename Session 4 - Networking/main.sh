77b1defd5f8de2e5fac927d8ea73f7e8c37167a1
sambhavbohra <sammybohra1008@gmail.com>
2026-08-25 18:41:24 +0530

    Refactor code structure for improved readability and maintainability

---
+++ Session 4 - Networking/main.sh
@@ -0,0 +1,18 @@
+ls
+hostname
+whoami
+ip a #shows IP address it is unique number for each device
+
+#four class for ip address 
+#A.B.C.D
+# 1 ───── 126    → Class A
+# 128 ─── 191    → Class B
+# 192 ─── 223    → Class C
+# 224 ─── 239    → Class D
+
+# 127.x.x.x      → Loopback
+# 240 ─── 255    → Reserved / Class E
+
+#subnettask - to diff -> host part and network part
+
+
